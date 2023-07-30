1ac02bea389ce826bbf8ae56655000e107f0baef
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-07-30 21:39:47 +0100

    Added Home, Players, Teams pages

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import { Button } from "@nextui-org/react";
+import './Home.css'
+
+const Home = () => {
+    return(
+        <div className="home-container">
+            <h1>Home</h1>
+            <Button>Button</Button>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/Players.js
@@ -0,0 +1,9 @@
+const Players = () => {
+    return(
+        <div>
+            <h1>Players</h1>
+        </div>
+    )
+}
+
+export default Players;
