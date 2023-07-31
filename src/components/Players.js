7a7baadec10ac2ebef676cc0633ea4bb1dd727b1
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-07-31 22:17:23 +0100

    Removed sort function: sorting error

--- src/components/Players.js
+++ src/components/Players.js
@@ -3,21 +3,7 @@ import { Table, useAsyncList, useCollator } from "@nextui-org/react";
 import playerData from "../PlayerData";
 
 const Players = () => {
-  const collator = useCollator({ numeric: true });
 
-  async function tableSort({ items, sortDescriptor }) {
-    return {
-      items: items.slice().sort((a, b) => {
-        let first = a[sortDescriptor.column];
-        let second = b[sortDescriptor.column];
-        let cmp = collator.compare(first, second);
-        if (sortDescriptor.direction === "descending") {
-          cmp *= -1;
-        }
-        return cmp;
-      }),
-    };
-  }
 
   const columns = [
     {
@@ -34,18 +20,8 @@ const Players = () => {
     },
   ];
 
-  const initialPlayersByPoints = playerData.sort((a,b) => b.total_points - a.total_points).slice(0, 20);
-  const [playersByPoints, setPlayersByPoints] = useState(initialPlayersByPoints);
-  const [sortDescriptor, setSortDescriptor] = useState({ column: "total_points", direction: "descending" });
+  const playersByPoints = playerData.sort((a,b) => b.total_points - a.total_points).slice(0, 20);
 
-
-  const handleSortChange = async () => {
-    const newDirection = sortDescriptor.direction === "descending" ? "ascending" : "descending";
-    setSortDescriptor({ ...sortDescriptor, direction: newDirection });
-
-    const { items } = await tableSort({ items: playersByPoints, sortDescriptor: { ...sortDescriptor, direction: newDirection } });
-    setPlayersByPoints(items);
-  };
 
   return (
     <div>
@@ -53,15 +29,13 @@ const Players = () => {
       <Table
         aria-label="Example table with dynamic content"
         css={{ height: "auto", minWidth: "100%" }}
-        // Use the handleSortChange function as the custom onSortChange handler
-        onSortChange={handleSortChange}
+
       >
         <Table.Header columns={columns}>
-          {(column) => <Table.Column key={column.key} allowsSorting>{column.label}</Table.Column>}
+          {(column) => <Table.Column key={column.key} >{column.label}</Table.Column>}
         </Table.Header>
         <Table.Body
           items={playersByPoints}
-          loadingState={useAsyncList.INITIAL_LOADING}
         >
           {(item) => (
             <Table.Row key={item.name}>
