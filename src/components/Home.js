4f824d65e165e30d9a098e58e70d0c5e7ae5089d
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-07-31 19:19:43 +0100

    Added home video

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
-import { Button } from "@nextui-org/react";
 import './Home.css'
+import video from '../assets/video.mp4'
 
 const Home = () => {
     return(
         <div className="home-container">
-            <h1>Home</h1>
-            <Button>Button</Button>
+            <div className='video-container'>
+                <video autoPlay loop muted width='1000' style={{ marginTop : 10}}>
+                    <source src={video} type='video/mp4'/>
+                </video>
+            </div>
+            <div className='text-container'>
+                <h1>Kick off your fantasy journey!</h1>
+                <h2>Your ultimate destination for all things football and stats</h2>
+            </div>
         </div>
     )
 }
