54506037c6cd7ca824788e3c97e5b9c33f7a8e14
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-07-31 19:18:55 +0100

    Added inline styling navbar

--- src/App.js
+++ src/App.js
@@ -1,63 +1,45 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Players from './components/Players';
 import Teams from './components/Teams';
-import { Button, Navbar} from '@nextui-org/react';
+import { Button, Navbar, useTheme} from '@nextui-org/react';
 import logo from './assets/pl-main-logo.png';
+import hero from './assets/hero.png'
 
 
 
 function App() {
 
-  const collapseItems = [
-    "Home",
-    "Players",
-    "Teams",
-    "Login",
-    "Sign Up",
-  ];
 
   return (
     <div className="App">
       <BrowserRouter>
+      <img src={hero} style={{ marginTop : 10, height: 120 }} alt='hero'></img>
 
-      <Navbar className='navbar' isBordered isCompact variant={"floating"} disableShadow={false} disableBlur={false}>
+      <Navbar className='navbar' isBordered={useTheme()} isCompact variant={"floating"} disableShadow={true} disableBlur={false} 
+              css={{$$navbarBlurBackgroundColor: "linear-gradient(90deg, rgba(28,30,31,1) 0%, rgba(133,30,122,1) 50%, rgba(28,30,31,1) 100%)",background: "rgb(24, 26, 27)",marginTop:'-4vh'}}>
         <Navbar.Brand>
-          <Navbar.Toggle aria-label="toggle navigation" />
           <img src={logo} height = '100vh' style={{ marginTop : 50 }} alt='pl-logo'></img>
           <h1 b color="inherit" hideIn="xs" style={{ marginLeft : 20}} >Fantasy</h1>
         </Navbar.Brand>
-        <Navbar.Content enableCursorHighlight activeColor={"success"} variant={"highlight"} hideIn={"xs"}>
+        <Navbar.Content enableCursorHighlight activeColor={"secondary"} variant={"highlight-solid"} hideIn={"xs"}>
           <Navbar.Link href="/">Home</Navbar.Link>
           <Navbar.Link href="/players">Players</Navbar.Link>
           <Navbar.Link href="/teams">Teams</Navbar.Link>
         </Navbar.Content>
-        <Navbar.Content enableCursorHighlight activeColor={"success"} variant={"highlight"} hideIn={"xs"}>
-          <Navbar.Link color="inherit" href="#">
-            Login
-          </Navbar.Link>
+        <Navbar.Content enableCursorHighlight activeColor={"secondary"} variant={"highlight-solid"} hideIn={"xs"}>
           <Navbar.Item >
-            <Button auto flat as={Link} color={"default"} href="#">
-              Sign Up
-            </Button>
-          </Navbar.Item>
+              <Button color="gradient" auto href="#" style={{ marginRight : 10}}>
+                Login
+              </Button>
+            </Navbar.Item>
+            <Navbar.Item >
+              <Button color="gradient" auto href="#">
+                Sign Up
+              </Button>
+            </Navbar.Item>
         </Navbar.Content>
-        <Navbar.Collapse>
-        {collapseItems.map((item, index) => (
-          <Navbar.CollapseItem key={item}>
-            <Link
-              color="inherit"
-              css={{
-                minWidth: "100%",
-              }}
-              href="/players"
-            >
-              {item}
-            </Link>
-          </Navbar.CollapseItem>
-        ))}
-        </Navbar.Collapse>
       </Navbar> 
 
       {/* <ul className='navbar'>
