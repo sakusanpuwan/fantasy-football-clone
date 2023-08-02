38cbef9e8a313c9d2aceea3af2e3fabff99a4bd3
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-08-02 17:17:08 +0100

    Added player card carousel

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,6 +1,9 @@
 import { useState } from "react"; // Import useState hook
 import { Table, useAsyncList, useCollator } from "@nextui-org/react";
-import playerData from "../PlayerData";
+import playerData from "../data/PlayerData";
+import Carousel from "react-material-ui-carousel";
+import { Paper } from "@mui/material";
+import PlayerCard from "./PlayerCard";
 
 const Players = () => {
 
@@ -18,25 +21,37 @@ const Players = () => {
       key: "goals_scored",
       label: "GOALS",
     },
+    {
+      key: "assists",
+      label: "ASSISTS",
+    },
+    {
+      key: "clean_sheets",
+      label: "CLEAN SHEETS",
+    }
   ];
 
   const playersByPoints = playerData.sort((a,b) => b.total_points - a.total_points).slice(0, 20);
 
 
   return (
-    <div>
+    <div className="players-container">
       <h1>Players</h1>
-      <Table
-        aria-label="Example table with dynamic content"
-        css={{ height: "auto", minWidth: "100%" }}
+      <div classname="carousel-container" >
+        <Carousel className="carousel">
+          {playersByPoints.slice(0,5).map((player) => {
+            return (
+              <PlayerCard player={player}></PlayerCard>
+            )
+          })}
+        </Carousel>
+      </div>
 
-      >
+      <Table isStriped removeWrapper css={{ height: "auto"}}>
         <Table.Header columns={columns}>
           {(column) => <Table.Column key={column.key} >{column.label}</Table.Column>}
         </Table.Header>
-        <Table.Body
-          items={playersByPoints}
-        >
+        <Table.Body items={playersByPoints}>
           {(item) => (
             <Table.Row key={item.name}>
               {(columnKey) => <Table.Cell>{item[columnKey]}</Table.Cell>}
