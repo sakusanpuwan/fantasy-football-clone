2e7b453bb7de0bd522491ee05e3de5601ae42095
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-08-04 17:51:24 +0100

    Added data grid of player data

--- src/components/Players.js
+++ src/components/Players.js
@@ -4,42 +4,77 @@ import playerData from "../data/PlayerData";
 import Carousel from "react-material-ui-carousel";
 import { Paper } from "@mui/material";
 import PlayerCard from "./PlayerCard";
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import './Players.css'
+
 
 const Players = () => {
 
 
   const columns = [
     {
-      key: "web_name",
-      label: "NAME",
+      field: 'code',
+      headerName: 'Image',
+      width: 100,
+      headerClassName: 'super-app-theme--header',
+      headerAlign: 'center',
+      renderCell: (player) => <img src={`https://resources.premierleague.com/premierleague/photos/players/250x250/p${player.value}.png`} height={75}/>, // renderCell will render the component
+    },
+    { 
+      field: 'second_name', 
+      headerName: 'Name',
+      width: 100,
+      headerClassName: 'super-app-theme--header',
+      headerAlign: 'center'
     },
     {
-      key: "total_points",
-      label: "POINTS",
+      field: 'total_points',
+      headerName: 'Points',
+      type: 'number',
+      headerClassName: 'super-app-theme--header',
+      headerAlign: 'center',
+      width: 100,
     },
     {
-      key: "goals_scored",
-      label: "GOALS",
-    },
-    {
-      key: "assists",
-      label: "ASSISTS",
-    },
-    {
-      key: "clean_sheets",
-      label: "CLEAN SHEETS",
-    }
-  ];
+      field: 'goals_scored',
+      headerName: 'Goals',
+      type: 'number',
+      headerClassName: 'super-app-theme--header',
+      headerAlign: 'center',
+      width: 100
 
-  const playersByPoints = playerData.sort((a,b) => b.total_points - a.total_points).slice(0, 20);
+    },
+    {
+      field: 'assists',
+      headerName: 'Assists',
+      type: 'number',
+      headerClassName: 'super-app-theme--header',
+      headerAlign: 'center',
+      width: 100
+
+    },
+    {
+      field: 'clean_sheets',
+      headerName: 'Clean Sheets',
+      type: 'number',
+      headerClassName: 'super-app-theme--header',
+      headerAlign: 'center',
+      width: 100
+
+    },
+    
+
+  ];
+  
+
+  const playersByPoints = playerData.sort((a,b) => b.total_points - a.total_points).slice(0, 200);
 
 
   return (
     <div className="players-container">
-      <h1>Players</h1>
       <div classname="carousel-container" >
         <Carousel className="carousel">
-          {playersByPoints.slice(0,5).map((player) => {
+          {playersByPoints.slice(0,10).map((player) => {
             return (
               <PlayerCard player={player}></PlayerCard>
             )
@@ -47,18 +82,23 @@ const Players = () => {
         </Carousel>
       </div>
 
-      <Table isStriped removeWrapper css={{ height: "auto"}}>
-        <Table.Header columns={columns}>
-          {(column) => <Table.Column key={column.key} >{column.label}</Table.Column>}
-        </Table.Header>
-        <Table.Body items={playersByPoints}>
-          {(item) => (
-            <Table.Row key={item.name}>
-              {(columnKey) => <Table.Cell>{item[columnKey]}</Table.Cell>}
-            </Table.Row>
-          )}
-        </Table.Body>
-      </Table>
+      <div className="table-container" style={{width: '50%',margin:'auto' }}>
+        <DataGrid
+          className="table"
+          rows={playersByPoints}
+          getRowHeight={() => 'auto'}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 20 },
+            },
+          }}
+          pageSizeOptions={[5, 10, 25]}
+          density="standard"
+        />
+      </div>
+
+
     </div>
   );
 };
